ebf7d31288422b2569a5dd72536e88b6da81167b
Juan pablo Patiño <juanpablo310446@gmail.com>
2024-11-28 14:01:48 -0500

    Vite config changes

--- vite.config.js
+++ vite.config.js
@@ -3,14 +3,5 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      '/auth': {
-        target: 'http://localhost:3000',
-        changeOrigin: true,
-        secure: false,
-      },
-    },
-  },
+  plugins: [react()]
 })
